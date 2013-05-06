0869f2e9ed5cc991182dd485d3fc5a49706c50d8
Stefan <cwwlovelife@gmail.com>
2013-05-06 17:13:36 +0800

    modify

--- src/player/main.c
+++ src/player/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <string.h>
+#include "../include/radio.h"
 char *program = NULL;
 
 void usage(void)
@@ -10,12 +11,41 @@ void usage(void)
 
 int main(int argc, char **argv)
 {
-	program = argv[0];
-
 	if(argc != 2) {
 		usage();
 		exit(1);
+	}
+    
+	int n,ret,i=0;
+	FILE *config_pf=NULL;
+	int file_fd;
+
+	char *pline=NULL;
+	radio_header file_header;
+	radio_config   radio_config_arr[2]={0};
+	program = argv[0];
+
+	bzero(&file_header, sizeof(file_header));
+
+	config_pf=fopen(argv[1],"rb");
+	if(config_pf==NULL)
+	{
+		perror("fopen");
+	}
+	
+	while((ret=getline(&pline ,&n ,config_pf))>0)
+	{
+		if(strncmp(pline,"#",1)!=0)
+		{
+			sscanf(pline,"%d %s %d %[0-9,.]:%d",&radio_config_arr[i].channel,radio_config_arr[i].fname,&radio_config_arr[i].bandwidth,&radio_config_arr[i].ip,&radio_config_arr[i].port);
+			i++;
+		}
 	}	
+
+	fclose(config_pf);
+
+
+
 
 	return 0;
 }	
