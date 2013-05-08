8cf9590408c4175e15f994b226343670df90cb66
Stefan <cwwlovelife@gmail.com>
2013-05-08 09:48:12 +0800

    add

--- src/player/test.c
+++ src/player/test.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "../include/radio.h"
  #include <sys/types.h>     
         #include <sys/socket.h>
 #include<string.h>
@@ -10,17 +11,18 @@ int main()
 	int sockfd;
 	socklen_t len;
 	struct sockaddr_in selfaddr,conaddr;
-	char buf[100]={0};
+	radio_unit buf;
 	struct ip_mreq mreq;
 
 	bzero(&selfaddr,sizeof(selfaddr));
 	bzero(&conaddr,sizeof(conaddr));
+	bzero(&buf,sizeof(buf));
 
 	selfaddr.sin_family=AF_INET;
 	selfaddr.sin_port=htons(1235);
 	selfaddr.sin_addr.s_addr=htonl(INADDR_ANY);
 
-	len=sizeof(selfaddr);
+	len=sizeof(conaddr);
 
 	sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
@@ -33,10 +35,10 @@ int main()
 		
 	
 
-	recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr *)&conaddr,&len);
 	while(1)
 	{
-		printf("buf=%s\n",buf);
+		recvfrom(sockfd,(void *)&buf,sizeof(buf),0,(struct sockaddr *)&conaddr,&len);
+		printf("buf=%s\n",buf.data);
 		sleep(1);
 		}
 	return 0;
