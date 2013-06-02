6e219f307c148aeed74558fcd19db308af857581
Serval Lee <lishanchao@gmail.com>
2013-06-02 22:04:16 +0800

    Set parameters of radio.

--- src/receiver/u-boot-mini6410/common/cmd_radio.c
+++ src/receiver/u-boot-mini6410/common/cmd_radio.c
@@ -1,15 +1,29 @@
 #include <common.h>
 #include <command.h>
+#include <net.h>
 
 int do_radio (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
+	IPaddr_t ser_ip;
+	unsigned short ser_port;
+
+	if (argc != 3) {
+		printf("Wrong arguments!\n");
+		return -1;
+	}
+
+	ser_ip = string_to_ip(argv[1]);
+	ser_port = (unsigned short)simple_strtoul(argv[2], NULL, 10);
+
+	printf("Reading data from %s:%d\n",
+		argv[1], ser_port);
 	printf("Radio...\n");
 	return 0;
 }
 
 U_BOOT_CMD(
-	radio,	1,	0,	do_radio,
-	"radio - Do multicast radio\n",
+	radio,	3,	0,	do_radio,
+	"radio\t- Do multicast radio\n",
 	NULL
 );
 
